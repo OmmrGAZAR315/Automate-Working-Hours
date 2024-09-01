b57b60d2eb5a784c4821d404bf349a93e3ff4df2
OmmrGAZAR315 <omarahmedelgazzar600@gmail.com>
2024-09-01 10:33:25 +0300

    remove spreadsheet id

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import os
 import working_hours
 import write_to_sheet
 from convert_doc_to_pdf import export_pdf
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def main():
     docId = input("Enter the document ID: ")
     workPlacePath = input("Enter the path where you want to save the PDF and txt file: ")
 
     # save_path = input("Enter the path where you want to save the extracted text: ")
-    spreadsheet_id = '1e39iKmA1lWhW7GpA59O7Q0z2tFkEDNhpR5PL7T9jf84'
     sheet_name = input('Enter the sheet name ')  # Replace with the actual sheet name you want to append to
 
     if workPlacePath == "":
@@ -22,7 +23,7 @@ def main():
     workPlacePath = export_pdf(docId, workPlacePath)
     save_path = working_hours.extract_text_from_pdf(workPlacePath)
     values = write_to_sheet.read_from_file(save_path)
-    write_to_sheet.write_to_sheet(spreadsheet_id, values, sheet_name)
+    write_to_sheet.write_to_sheet(os.getenv('SPREADSHEET_ID'), values, sheet_name)
 
 
 if __name__ == "__main__":
--- setAccessToken.py
+++ setAccessToken.py
@@ -1,5 +1,4 @@
 import requests
-import dotenv
 import os
 from dotenv import load_dotenv
 
